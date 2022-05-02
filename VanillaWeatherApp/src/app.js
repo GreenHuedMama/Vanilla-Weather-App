9223b2d61b72cde95a609991aa9bca1bfdfefacd
GreenHuedMama <101072904+GreenHuedMama@users.noreply.github.com>
2022-05-02 17:51:52 -0500

    Added search engine functions

--- VanillaWeatherApp/src/app.js
+++ VanillaWeatherApp/src/app.js
@@ -46,15 +46,26 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "59ee4591990d1ea0e068edfbc69866c6";
-let city = "Miami";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "59ee4591990d1ea0e068edfbc69866c6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Miami");
 
 function showFahrenheit(event) {
   event.preventDefault();
-  let fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
